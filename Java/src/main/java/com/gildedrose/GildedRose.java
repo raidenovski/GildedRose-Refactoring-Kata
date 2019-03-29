677a7a0e0bd8af35238ec1181f48e4198f65caae
Uros Trifunovic <uros.trifunovic@adidas.com>
2019-03-29 22:32:03 +0100

    Add tests and begin refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,18 @@
 package com.gildedrose;
 
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
+import java.util.function.ToIntFunction;
+
 class GildedRose {
+
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String CONCERT_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,55 +20,72 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+
+        for (Item i : items) {
+
+            if (!isItemAgedBrie(i) && !isItemConcertPass(i)) {
+                if (i.quality > MIN_QUALITY) {
+                    if (!isItemSulfuras(i)) {
+                        i.quality = i.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (i.quality < MAX_QUALITY) {
+                    i.quality = i.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (isItemConcertPass(i)) {
+                        if (i.sellIn < 11) {
+                            if (i.quality < MAX_QUALITY) {
+                                i.quality = i.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (i.sellIn < 6) {
+                            if (i.quality < MAX_QUALITY) {
+                                i.quality = i.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isItemSulfuras(i)) {
+                i.sellIn = i.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (i.sellIn < 0) {
+                if (!isItemAgedBrie(i)) {
+                    if (!isItemConcertPass(i)) {
+                        if (i.quality > MIN_QUALITY) {
+                            if (!isItemSulfuras(i)) {
+                                i.quality = i.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        i.quality = i.quality - i.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (i.quality < MAX_QUALITY) {
+                        applyIntFunc(i, j -> j.quality + 1);
                     }
                 }
             }
         }
+    }
+
+    private boolean isItemAgedBrie(Item i) {
+        return AGED_BRIE.equals(i.name);
+    }
+
+    private boolean isItemConcertPass(Item i) {
+        return CONCERT_PASS.equals(i.name);
+    }
+
+    private boolean isItemSulfuras(Item i) {
+        return SULFURAS.equals(i.name);
+    }
+
+    private void applyIntFunc(Item i, ToIntFunction<Item> func) {
+        func.applyAsInt(i);
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,156 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    private Item[] items;
+
+    @BeforeEach
+    public void setup() {
+        this.items = ItemsUtils.items;
     }
+
+    @Nested
+    @DisplayName("Given normal item")
+    class NormalItemTest {
+
+        @Test
+        @DisplayName("When updating quality then update all properties")
+        public void whenUpdatingQualityThenUpdateAllProperties() {
+            Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20), };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(19, app.items[0].quality);
+            Assertions.assertEquals(9, app.items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("When Sell-In date expires then quality degrades twice as fast")
+        public void whenSellInDateExpiresThenQualityDegradesTwiceAsFast() {
+            final int expectedQuality = 7;
+            final int expectedSellIn = -1;
+            Item[] items = new Item[] { new Item("+5 Dexterity Vest", 1, 10), };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+            Assertions.assertEquals(expectedSellIn, app.items[0].sellIn);
+
+        }
+
+        @Test
+        @DisplayName("When quality degrades then it never drops bellow zero")
+        public void whenQualityDegradesThenItNeverDropsBellowZero() {
+            final int expectedQuality = 0;
+            Item[] items = new Item[] { new Item("+5 Dexterity Vest", 1, 1), };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+
+        }
+    }
+
+    @Nested
+    @DisplayName("Given Aged Brie")
+    class AgedBrie {
+
+        @Test
+        @DisplayName("When updating quality then increase instead of decrease")
+        public void whenUpdatingQualityThenIncreaseInsteadOfDecrease() {
+            final int expected = 1;
+            Item[] items = new Item[] { new Item("Aged Brie", 2, 0)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expected, app.items[0].quality);
+            Assertions.assertEquals(expected, app.items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("When increasing quality then don't increase over 50")
+        public void whenIncreasingQualityThenDontIncreaseOverFifty() {
+            final int expected = 50;
+            Item[] items = new Item[] { new Item("Aged Brie", 2, 50)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expected, app.items[0].quality);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Given Sulfuras")
+    class Sulfuras {
+
+        @Test
+        @DisplayName("When updating quality then never decrease")
+        public void whenUpdatingQUalityThenNeverDecarease() {
+            final int expected = 20;
+            Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 20)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expected, app.items[0].quality);
+        }
+    }
+
+    @Nested
+    @DisplayName("Given Backstage Pass")
+    class BackstagePass {
+
+        @Test
+        @DisplayName("When there are more than 10 days to conecrt then increase quality by 1")
+        public void whenThereAreMoreThan10DaysToConcertThenIncreaseQualityByOne() {
+            final int expectedQuality = 21;
+            final int expectedSellIn = 14;
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+            Assertions.assertEquals(expectedSellIn, app.items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("When there are between 10 and 5 days to concert then increase quality by 2")
+        public void whenThereAreInBetween10And5DaysThenIncreaseQualityBy2() {
+            final int expectedQuality = 22;
+            final int expectedSellIn = 8;
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+            Assertions.assertEquals(expectedSellIn, app.items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("When there are less than 5 days to concert then increase quality by 3")
+        public void whenThereAreLessThan5DaysThenIncreaseQualityBy3() {
+            final int expectedQuality = 23;
+            final int expectedSellIn = 3;
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+            Assertions.assertEquals(expectedSellIn, app.items[0].sellIn);
+        }
+
+        @Test
+        @DisplayName("After the concert, drop quality  to 0")
+        public void whenSellInDateIsExpiredThenDropQualityToZero() {
+            final int expectedQuality = 0;
+            final int expectedSellIn = -1;
+            Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            Assertions.assertEquals(expectedQuality, app.items[0].quality);
+            Assertions.assertEquals(expectedSellIn, app.items[0].sellIn);
+        }
+
+        }
 
 }
